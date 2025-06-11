11fdf08c38206461d574c9ef40e878d6e17a4b2d
Aaron Ellington <aaron@ellington.io>
2025-06-11 12:44:24 -0400

    clean up poc

--- src/app.tsx
+++ src/app.tsx
@@ -1,32 +1,24 @@
 import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
 import './app.css'
 
 export function App() {
   const [serverAddress, setServerAddress] = useState('http://localhost:2291')
-  const [serverResult, setServerResult] = useState('')
+  const [serverResult, setServerResult] = useState<string | null>(null)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
       <div class="card">
+        <h1>HTTPlay.dev</h1>
         <div>
-
           <label >
             Server Address:
+          </label>
+          <div>
+
             <input onChange={(e) => {
               setServerAddress((e.target as any).value ?? '')
             }} value={serverAddress} />
-          </label>
+          </div>
         </div>
         <button onClick={() => {
           fetch(serverAddress).then((response) => {
@@ -39,25 +31,15 @@ export function App() {
           })
         }
         }>
-          Server Result: {serverResult}
+          Refresh
         </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
+        <div>
+          Server Result:
+          <pre>
+            {JSON.stringify(serverResult)}
+          </pre>
+        </div>
       </div>
-      <p>
-        Check out{' '}
-        <a
-          href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
-          target="_blank"
-        >
-          create-preact
-        </a>
-        , the official Preact + Vite starter
-      </p>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
     </>
   )
 }
